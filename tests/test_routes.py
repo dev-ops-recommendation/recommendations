4ade906f1c21372ce140925d7af02a185c6c2ade
Sarah Lin <xiaowei.lin@outlook.com>
2021-07-06 12:33:29 -0400

    support list recommendations

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -112,36 +112,5 @@ class TestRecommendationServer(TestCase):
     
     def test_list_recommendations(self):
         """Get a list of recommendations"""
-        self._list_recommendations(4)
-        resp = self.app.get(BASE_URL)
+        resp = self.app.get("/recommendations")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        data = resp.get_json()
-        self.assertEqual(len(data),4)
-
-
-    def test_list_by_recommendation_type(self):
-        """List Recommendations by recommendation_type"""
-        recommendations = self._create_recommendations(10)
-        test_recommendation_type = recommendations[0].recommendation_type
-        recommendation_type_recommendations = [recommendation for recommendation in recommendations if recommendation.recommendation_type == test_recommendation_type]
-        resp = self.app.get(BASE_URL, query_string="recommendation_type={}".format(test_recommendation_type))
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        data = resp.get_json()
-        self.assertEqual(len(data), len(recommendation_type_recommendations))
-        # check the data just to be sure
-        for recommendation in data:
-            self.assertEqual(recommendation["recommendation_type"], test_recommendation_type)
-    
-    def test_list_recommendation_by_active(self):
-        """List Recommendations by Active"""
-        recommendations = self._create_recommendations(10)
-        test_active = recommendations[0].active
-        active_recommendations = [recommendation for recommendation in recommendations if recommendation.active == test_active]
-        resp = self.app.get(BASE_URL, query_string="active={}".format(test_active))
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        data = resp.get_json()
-        self.assertEqual(len(data), len(active_recommendations))
-        # check the data just to be sure
-        for recommendation in data:
-            self.assertEqual(recommendation["active"], test_active)
-
